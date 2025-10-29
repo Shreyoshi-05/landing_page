157305848e8c87e78b279cb4bb460049b83d3b12
Shreyaoshi Das <sdas200010@gmail.com>
2025-10-29 21:29:05 +0530

    added responsive nav

--- src/component/pages/Nav.jsx
+++ src/component/pages/Nav.jsx
@@ -1,13 +1,18 @@
-import React, { useEffect, useState } from 'react'
-import '../css/Nav.css'
-import { Link } from 'react-router-dom'
+import React, { useEffect, useState } from "react";
+import "../css/Nav.css";
+import { Link } from "react-router-dom";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { RxCross2 } from "react-icons/rx";
+import { ImCross } from "react-icons/im";
 
 const Nav = () => {
   const [scrolled, setScrolled] = useState(false);
+  const [smallSize, setSmallSize] = useState(false);
+  const [open, setOpen] = useState(false);
 
   useEffect(() => {
     const handleScroll = () => {
-      if (window.scrollY > 50) {
+      if (window.scrollY > 500) {
         setScrolled(true);
       } else {
         setScrolled(false);
@@ -18,13 +23,46 @@ const Nav = () => {
     return () => window.removeEventListener("scroll", handleScroll);
   }, []);
 
-  return (
-    <div className={`nav ${scrolled ? "scrolled" : ""}`} >
-      <div className="logo">
-        <img src="/logo.png" alt="" />
-      </div>
-      <div className="menu">
+  useEffect(() => {
+    function getSize() {
+      console.log(window.innerWidth);
+      let size = window.innerWidth;
+      if (size < 900) {
+        setSmallSize(true);
+      } else {
+        setSmallSize(false);
+      }
+    }
 
+    window.addEventListener("resize", getSize);
+    return () => removeEventListener("resize", getSize);
+  }, []);
+
+  console.log(open);
+
+  return (
+    <div className="nav_container">
+      <div className={`nav ${scrolled ? "scrolled" : ""}`}>
+      {smallSize ? (
+        <div className="logo active">
+          {open ? (
+            <ImCross size={26} color="white" onClick={() => setOpen(false)} />
+          ) : (
+            <GiHamburgerMenu
+              size={26}
+              color="white"
+              onClick={() => setOpen(true)}
+            />
+          )}
+          <img src="/logo.png" alt="" />
+        </div>
+      ) : (
+        <div className="logo">
+          <img src="/logo.png" alt="" />
+        </div>
+      )}
+
+      <div className="menu">
         <Link to="#">me</Link>
         <Link to="#">About</Link>
         <Link to="#">Packages</Link>
@@ -34,8 +72,19 @@ const Nav = () => {
       <div className="btn">
         <button>Log In</button>
       </div>
+      
     </div>
-  )
-}
+    {
+      open && <div className="dropdown">
+        <Link to="#">me</Link>
+        <Link to="#">About</Link>
+        <Link to="#">Packages</Link>
+        <Link to="#">Gallery</Link>
+        <Link to="#">Contact</Link>
+      </div>
+    }
+    </div>
+  );
+};
 
-export default Nav
+export default Nav;
